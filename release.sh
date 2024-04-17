dadf43da556a00f7bacd71cb154f2932757d9144
ganler <jaway.liu@gmail.com>
2024-04-17 04:51:24 -0500

    feat: add script to upload to HF

--- release.sh
+++ release.sh
@@ -10,3 +10,4 @@ fi
 git tag -a $version -m "MBPP+ $version"
 git push --tags
 gh release create $version $(dirname $0)/*.jsonl.gz --title "MBPP+ $version" --notes "MBPP+ $version"
+python hf_upload.py --version "$version" --overwrite
